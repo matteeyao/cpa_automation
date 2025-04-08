2183aa06eadf8226c76ee62f05ae0a52163bdb43
Matt Yao <myao@8thlight.com>
2025-04-07 21:06:36 -0500

    Add project ids to apis

--- terraform/apis.tf
+++ terraform/apis.tf
@@ -1,29 +1,35 @@
 resource "google_project_service" "datastream" {
+  project            = var.project_id
   service            = "datastream.googleapis.com"
   disable_on_destroy = false
 }
 
 resource "google_project_service" "bigquery" {
+  project            = var.project_id
   service            = "bigquery.googleapis.com"
   disable_on_destroy = false
 }
 
 resource "google_project_service" "cloudsql" {
+  project            = var.project_id
   service            = "sqladmin.googleapis.com"
   disable_on_destroy = false
 }
 
 resource "google_project_service" "compute" {
+  project            = var.project_id
   service            = "compute.googleapis.com"
   disable_on_destroy = false
 }
 
 resource "google_project_service" "cloudresourcemanager" {
+  project            = var.project_id
   service            = "cloudresourcemanager.googleapis.com"
   disable_on_destroy = false
 }
 
 resource "google_project_service" "servicenetworking" {
+  project            = var.project_id
   service            = "servicenetworking.googleapis.com"
   disable_on_destroy = false
 }
